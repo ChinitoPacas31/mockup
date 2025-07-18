e9c0f20540764074a7ac93a3b80276ad060c5a60
David Bustillos. <136139565+DavidJBM@users.noreply.github.com>
2025-07-17 20:28:03 -0600

    si que siii
    asu ptm

--- myapp/config.js
+++ myapp/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = 'http://10.100.1.29:5000';
+const API_BASE_URL = 'http://10.100.0.105:5000';
 // const API_BASE_URL = 'http://10.100.0.71:5000';
 export default API_BASE_URL;
