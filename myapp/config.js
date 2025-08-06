0e6a9e5ec399f198634da37b62172417f203a9d4
logicalCODE28 <luiscdombanda@gmail.com>
2025-08-06 12:32:13 -0600

    si
    si

--- myapp/config.js
+++ myapp/config.js
@@ -1,4 +1,4 @@
 // config.js
-//const API_BASE_URL = 'http://10.100.0.95:5000';
-const API_BASE_URL = 'http://192.168.100.9:5000';
+const API_BASE_URL = 'http://10.100.0.25:5000';
+//const API_BASE_URL = 'http://192.168.100.9:5000';
 export default API_BASE_URL;
